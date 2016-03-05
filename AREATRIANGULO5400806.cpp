1dea11f8d3d731e30b80c3c9984269b06eaa8093
RUSOE <abismo_aguiar96@hotmail.com>
2016-03-04 23:47:28 -0600

    Update AREATRIANGULO5400806.cpp

--- AREATRIANGULO5400806.cpp
+++ AREATRIANGULO5400806.cpp
@@ -1,22 +1,21 @@
-//Calcula el area de un circulo
 /*
-Hecho por Alfredo Aguiar Arce
+"Calcula el area de un circulo"
+Hecho por Alfredo Aguiar Arce.
+Ingenieria en mecatronica.
 */
 #include <iostream.h>
 #include <conio.h>
 
-
 float b,h,A;
-
 void main()
 {
- 	cout<<"Proporciona valor de base : ";
-  	cin>>b;
-   cout<<"Proporciona valor de bla altura : ";
-   cin>>h;
-
+	cout<<"Proporciona valor de base : ";
+	cin>>b;
+	
+   	cout<<"Proporciona valor de bla altura : ";
+   	cin>>h;
+   	
 	A = ((b*h)/2);
 	cout<<"el area del triangulo es : "<<A;
 	getch();
-
 }
