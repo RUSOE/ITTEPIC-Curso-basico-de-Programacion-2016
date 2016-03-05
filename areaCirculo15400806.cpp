0feb2e5c9f49086607091dc99f27b5e09d2efcd1
RUSOE <abismo_aguiar96@hotmail.com>
2016-03-05 00:33:52 -0600

    Update areaCirculo15400806.cpp

--- areaCirculo15400806.cpp
+++ areaCirculo15400806.cpp
@@ -1,18 +1,19 @@
-//Calcula el area de un circulo
 /*
+Calcula el area de un circulo
 Hecho por Alfredo Aguiar Arce
+Intituto Tecnologico de Tepic
 */
 #include <iostream.h>
 #include <conio.h>
 
-
 float r,ac;
-
 void main()
 {
  	cout<<"Proporciona valor radio : ";
   	cin>>r;
-	ac = 3.14*(r*r);
+  	
+	ac = 3.14*(r*r);//Operacion para calcular el area del circulo
 	cout<<"el area del circulo es : "<<ac;
+	
 	getch();
 }
