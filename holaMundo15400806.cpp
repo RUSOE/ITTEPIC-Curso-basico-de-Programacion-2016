3470b3f213a710616e589af7b85c44870fabf798
RUSOE <abismo_aguiar96@hotmail.com>
2016-03-04 23:54:43 -0600

    Update and rename holamundo15400806.cpp to holaMundo15400806.cpp

---
+++ holaMundo15400806.cpp
@@ -0,0 +1,19 @@
+/*
+"Clase de introduccion"
+Hecho por Alfredo Aguiar Arce
+Ingenieria en Mecatronica
+Intituto tecnologico de tepic
+*/
+
+//Librerias usadas para  el programa
+#include <iostream.h>
+#include <conio.h>
+
+void main(){
+
+      cout<<"hola mundo soy de mecatronica"<<endl;
+      cout<<"soy de mecatronica";
+      
+      //esta funcion pausa el programa
+      getch();
+}
--- holamundo15400806.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream.h>
-#include <conio.h>
-void main(){
-
-      cout<<"hola mundo soy de mecatronica"<<endl;
-      cout<<"soy de mecatronica";
-
-	getch();
-}
