119b48bdb5dd29c695ac66055135207d180fdce7
RUSOE <abismo_aguiar96@hotmail.com>
2016-03-04 23:51:26 -0600

    Update AREATRAPECIO15400806.cpp

--- AREATRAPECIO15400806.cpp
+++ AREATRAPECIO15400806.cpp
@@ -1,25 +1,27 @@
-//Calcula el area de un circulo
 /*
+"Calcula el area de un Trapecio"
 Hecho por Alfredo Aguiar Arce
+Ingenieria en mecatronica
+Instituto tecnologico de Tepic
 */
 #include <iostream.h>
 #include <conio.h>
 
-
 float b,B,h,At;
-
 void main()
 {
  	cout<<"Proporciona valor de base Menor : ";
   	cin>>b;
-   cout<<"Proporciona valor de base Mayor : ";
-   cin>>B;
-   cout<<"Proporciona valor de bla altura : ";
-   cin>>h;
+  	
+  	cout<<"Proporciona valor de base Mayor : ";
+  	cin>>B;
+  	
+  	cout<<"Proporciona valor de bla altura : ";
+   	cin>>h;
 
+//Realiza la operacion para calcular el area del trapecio (((BASE MAYOR+ MASE MENOR)*ALTURA)/2)
 	At = (((b+B)*h)/2);
 	cout<<"el area del Trapecio es : "<<At;
+	
 	getch();
-
-
 }
