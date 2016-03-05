8d83d9aca68d7d85a98227d8589527a340d54189
RUSOE <abismo_aguiar96@hotmail.com>
2016-03-04 23:56:14 -0600

    Update and rename AREATRIANGULO5400806.cpp to AreaTriangulo15400806.cpp

--- AreaTriangulo15400806.cpp
+++ AreaTriangulo15400806.cpp
@@ -15,7 +15,9 @@ void main()
    	cout<<"Proporciona valor de bla altura : ";
    	cin>>h;
    	
+   	//Formula para area de triangulo ((Base*Altura)/2)
 	A = ((b*h)/2);
 	cout<<"el area del triangulo es : "<<A;
+	
 	getch();
 }
