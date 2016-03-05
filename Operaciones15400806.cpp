1cd001221546bcde2cebe10c336b5b944d8f5eb8
RUSOE <abismo_aguiar96@hotmail.com>
2016-03-05 00:41:15 -0600

    Update Operaciones15400806.cpp

--- Operaciones15400806.cpp
+++ Operaciones15400806.cpp
@@ -1,24 +1,35 @@
+/*
+Obtener 2 numero y realizar dependiendo de el valor una operacion donde
+	1)SUMA
+	2)RESTA
+	3)MULTIPLICACION
+	4)DIVISION
+	
+Hecho por Alfredo Aguiar Arce
+Intituto Tecnologico de Tepic
+*/
 #include <conio.h>
 #include <iostream.h>
 #include <utility>
 
-
 void main(){
-  //Declare the function
+  	//Declarar la funcion que realiza las operaciones
 	int funcion(int a,int b,int operation);
 	int n,m,o,result;
+	
+	cout<<"Indica dos numeros"<<endl;
+	cin>>n>>m;//tomamos los 2 numeros en un comando
+	cout<<"Que deseas hacer con ellos 1)suma 2)resta 3)multipliacion 4)division"<<endl;
+	cin>>o;
 
-
-
-   cout<<"Indica dos numeros"<<endl;
-   cin>>n>>m;
-   cout<<"Que deseas hacer con ellos 1)suma 2)resta 3)multipliacion 4)division"<<endl;
-   cin>>o;
-
-   	if(o>0 && 0<=4){
-   		result= funcion(n,m,o);
+   	if(o>0 && 0<=4){//Comprobamos que 'o' este dentro del rango 1-4
+   	
+   		//Llamamos ala funcion que regresa el valor de la operacion realizada y este es almacenado en la variable result
+   		result = funcion(n,m,o);
    		cout<<result<<endl;
-   	}else{
+   		
+   	}else{//no cumplio el rango 1-4
+   	
    		cout<<"comando invalido"<<endl;
    	}
 
@@ -26,28 +37,30 @@ void main(){
 
 }
 
+
+/*
+	Esta funcion hace mas facil el hecho de realizar la operacion
+	donde 'a' y 'b' son los valores a tratar  'o'indica el numero que indicara que operacion realizar
+*/
 int funcion(int a,int b,int operation)
 {
-	int total;
+	int total;//Variable que almacena el valor que regresara
     	switch(operation){
       	case 1:
-      			total= a+b;
-      	break;
-
+      		total= a+b;
+      		break;
        	case 2:
-      	 		total= a-b;
-     	   break;
-
-     		 case 3:
-     		 	total=a*b;
-     		 break;
-
+      	 	total= a-b;
+      	 	break;
+     	 case 3:
+     		total=a*b;
+     		break;
       	case 4:
       		total= a/b;
-         break;
-
+         	break;
   	 }
-   return total;
+  	 
+	return total;//Valor que regresa la funcion luego de la operacion seleccionada
 }
 
 
