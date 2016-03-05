265b620ea5e4cdf6cc81b08dba399dd3ef6c4dd1
RUSOE <abismo_aguiar96@hotmail.com>
2016-03-05 00:30:49 -0600

    Update NumeroPositivoNegativoCero15400806.cpp

--- NumeroPositivoNegativoCero15400806.cpp
+++ NumeroPositivoNegativoCero15400806.cpp
@@ -1,14 +1,26 @@
+/*
+"Verificar si un numero es positivo negativo o cero"
+Hecho Por Alfredo Aguiar Arce
+Instituto Tecnologico de tepic
+*/
 #include <conio.h>
 #include <iostream.h>
 
 void main(){
 	int n;
-   cout<<"Indica un numero"<<endl;
-   cin>>n;
+   
+   	cout<<"Indica un numero"<<endl;
+   	cin>>n;
 
-   if(n==0){cout<<"El numero es cero"<<endl;}
-   else if(n>0){cout<<"El numero es positivo"<<endl;}
-   else if(n<0){cout<<"El numero es negativo"<<endl;}
+	 if(n==0)
+	 {
+	 	cout<<"El numero es cero"<<endl;
+	 }else if(n>0)
+	 {
+	 	cout<<"El numero es positivo"<<endl;
+	 }else if(n<0){
+	 	cout<<"El numero es negativo"<<endl;
+	 }
 
    getch();
 
